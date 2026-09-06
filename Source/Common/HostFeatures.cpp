0bfe063ea9d275660171a1e6bb8ef070d3d066da
yellowsparkles584 <154008927+yellowsparkles584@users.noreply.github.com>
2026-09-06 20:07:04 +1000

    •

--- Source/Common/HostFeatures.cpp
+++ Source/Common/HostFeatures.cpp
@@ -753,9 +753,6 @@ FEXCore::HostFeatures FetchHostFeatures() {
   FillMIDRInformationViaLinux(&HostFeatures);
   FetchHostFeatures(Features, HostFeatures, true, CTR, MIDR);
 
-<<<<<<< HEAD
-  HostFeatures.SupportsCPUIndexInTPIDRRO = false;
-=======
   HostFeatures.SupportsCPUIndexInTPIDRRO = true;
   HostFeatures.HostType = FEXCore::HostFeatures::HostTypeEnum::Linux;
 >>>>>>> 5855750e7 (•)
