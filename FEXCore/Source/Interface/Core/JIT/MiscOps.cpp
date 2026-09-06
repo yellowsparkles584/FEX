dfaf587d2195e618155ccdc0edcf9c798a6233f2
yellowsparkles584 <154008927+yellowsparkles584@users.noreply.github.com>
2026-09-06 19:55:39 +1000

    •

--- FEXCore/Source/Interface/Core/JIT/MiscOps.cpp
+++ FEXCore/Source/Interface/Core/JIT/MiscOps.cpp
@@ -231,7 +231,7 @@ DEF_OP(PrintMsg) {
 
 DEF_OP(ProcessorID) {
   if (CTX->HostFeatures.SupportsCPUIndexInTPIDRRO) {
-    ldr(ARMEmitter::Size::i64Bit, GetReg(Node), ARMEmitter::MemOperandAL(STATE, offsetof(FEXCore::Core::CpuStateFrame, Thread)));
+    mov(ARMEmitter::Size::i64Bit, GetReg(Node), 0);
     return;
   }
 #ifdef _WIN32
